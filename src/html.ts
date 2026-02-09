a3cdc37920c64893dff34481662703cb55d4ca12
Eric Dauenhauer <eric@ericyd.com>
2026-02-09 14:50:04 -0600

    expose HTML on all methods

--- src/html.ts
+++ src/html.ts
@@ -1,11 +1,18 @@
-import { H3 } from "h3";
+import { EventHandlerRequest, H3, H3Event } from "h3";
 
 export const app = new H3();
 
-app.get(
-  "/",
-  (event) =>
-    new Response("<html><body><h1>Hello, World!</h1></body></html>", {
-      headers: { "Content-Type": "text/html" },
-    })
-);
+app.get("/", html);
+app.post("/", html);
+app.put("/", html);
+app.delete("/", html);
+app.options("/", html);
+app.head("/", html);
+app.trace("/", html);
+app.connect("/", html);
+
+function html(_event: H3Event<EventHandlerRequest>): Response {
+  return new Response("<html><body><h1>Hello, World!</h1></body></html>", {
+    headers: { "Content-Type": "text/html" },
+  });
+}
